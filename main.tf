519e58435414f730f5a9cb7628ef7fb47bd5fb6c
Lincy Varghese <lincytv@gmail.com>
2021-09-25 21:19:20 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,28 +3,9 @@ provider "aws" {
 }
 
 resource "aws_instance" "demo-ec2" {
-  type = "t2.micro"
+  instance_type = "t2.micro"
   ami = "ami-0a57edb3631e68796"
   tags = {
     Name = "demo-club-house"
   }
 }
-
- terraform {
-      backend "remote" {
-        # The name of your Terraform Cloud organization.
-        organization = "example-organization"
-
-        # The name of the Terraform Cloud workspace to store Terraform state files in.
-        workspaces {
-          name = "example-workspace"
-        }
-      }
-    }
-
-    # An example resource that does nothing.
-    resource "null_resource" "example" {
-      triggers = {
-        value = "A example resource that does nothing!"
-      }
-    }
